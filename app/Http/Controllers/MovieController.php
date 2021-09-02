16b86ee5c6417f44a32470aac7c12e836c020456
FinnMaladay <finn.maladay@gmail.com>
2021-09-02 11:38:53 +1000

    Commit small changes

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('movies.movie', [
+            'movies' => Movie::all()
+        ]);
+    }
 }
