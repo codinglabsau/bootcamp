a87637ddd2c5755105a5542d6bd066b9455ba440
vrodriguero <vickyamauti@hotmail.com>
2021-09-03 12:12:50 +1000

    code formatting

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
+use App\Models\Movie;
+use App\Models\Review;
+use App\Models\Celebrity;
+use App\Models\GenreMovie;
+use App\Models\CelebrityMovie;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +19,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Genre::factory(10)->create();
-        \App\Models\Movie::factory(10)->create();
-        \App\Models\Review::factory(10)->create();
-        \App\Models\Celebrity::factory(10)->create();
-        \App\Models\CelebrityMovie::factory(10)->create();
-        \App\Models\GenreMovie::factory(10)->create();
+        Genre::factory(10)->create();
+        Movie::factory(10)->create();
+        Review::factory(10)->create();
+        Celebrity::factory(10)->create();
+        CelebrityMovie::factory(10)->create();
+        GenreMovie::factory(10)->create();
     }
 }
