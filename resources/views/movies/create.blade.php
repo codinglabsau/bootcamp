316b3d6dde17aea413b38048bc74bc6ee6721ab7
FinnMaladay <finn.maladay@gmail.com>
2021-10-14 09:26:24 +1000

    Add and edit pages updated
    Custom layout has been created for Add and Edit page.
    
    Functions are all identical as before
    
    Minor responsive implemented: Reduces columns of contents from 2 to 1 so all input text box appear in a single file
    
    Added in Celebrity and Genre TextArea for future Many to Many function implementation. Unsure if this is the best approach to take (how to ensure that movie or genre provide by Admin exists in database? Does a custom requirement need to be defined?)

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -6,88 +6,87 @@
     </x-slot>
     <div class="grid grid-cols-1">
         <div class="w-1/2 justify-self-center">
-            <x-form :action="route('movies.store')" method="POST" buttonLabel="CREATE" >
+            <x-form :action="route('movies.store')" method="POST" buttonLabel="CREATE">
                 @csrf
-                    <div class="flex grid grid-cols-2 gap-y-6 gap-x-2 place-content-between text-white border-pink-500 border-2 border-opacity-50 bg-purple-800 bg-opacity-25 px-4 py-6">
-                        <!-- cell 1-->
-                        <div class="grid grid-flow-row gap-y-2 justify-self-center">
-                            <!--Title-->
-                            <div>
-                                <x-label for="title" value="Title"/>
+                <div class="flex grid grid-cols-1 gap-y-6 gap-x-2 place-content-between text-white border-pink-500 border-2 border-opacity-50 bg-purple-800 bg-opacity-25 px-4 py-6 md:grid-cols-2">
+                    <!-- cell 1-->
+                    <div class="grid grid-flow-row gap-y-2 justify-self-center">
+                        <!--Title-->
+                        <div>
+                            <x-label for="title" value="Title"/>
 
-                                <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
-                                type="text"
-                                name="title"
-                                id="title"
-                                :value="old('title')"/>
+                            <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
+                            type="text"
+                            name="title"
+                            id="title"
+                            :value="old('title')"/>
 
-                            </div>
-                            <!--Release Date-->
-                            <div>
-                                <x-label for="release_date" value="Release Date"/>
-
-                                <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
-                                type="date"
-                                name="release_date"
-                                id="release_date"
-                                :value="old('release_date')"/>
-                            </div>
-                            <!--Poster-->
-                            <div>
-                                <x-label for="poster" value="Poster"/>
-
-                                <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
-                                type="text"
-                                name="poster"
-                                id="poster"
-                                :value="old('poster')"/>
-                            </div>
-                            <!--Trailer-->
-                            <div>
-                                <x-label for="trailer" value="Trailer"/>
-
-                                <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
-                                type="text"
-                                name="trailer"
-                                id="trailer"
-                                :value="old('trailer')"/>
-                            </div>
                         </div>
+                        <!--Release Date-->
+                        <div>
+                            <x-label for="release_date" value="Release Date"/>
 
-                        <!--cell 2-->
-                        <div class="justify-self-center grid grid-cols-1">
-                            <!--Blurb-->
-                            <div class="self-end">
-                                <x-label for="blurb" value="Blurb"/>
-
-                                <textarea class="leading-normal border-pink-500 border bg-purple-800 bg-opacity-25"
-                                name="blurb"
-                                id="blurb"></textarea>
-                            </div>
+                            <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
+                            type="date"
+                            name="release_date"
+                            id="release_date"
+                            :value="old('release_date')"/>
                         </div>
+                        <!--Poster-->
+                        <div>
+                            <x-label for="poster" value="Poster"/>
 
-                        <!--cell 3-->
-                        <div class="pb-8 justify-self-center">
-                            <!--Celebrities-->
-                            <x-label for="celebrity" value="Celebrities"/>
-
-                            <textarea class="leading-normal border-pink-500 border bg-purple-800 bg-opacity-25"
-                            name="celebrities"
-                            id="celebrities"></textarea>
+                            <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
+                            type="text"
+                            name="poster"
+                            id="poster"
+                            :value="old('poster')"/>
                         </div>
+                        <!--Trailer-->
+                        <div>
+                            <x-label for="trailer" value="Trailer"/>
 
-                        <!--cell 4-->
-                        <div class="pb-8 justify-self-center">
-                            <!--Genre-->
-                            <x-label for="genre" value="Genres"/>
-
-                            <textarea class="leading-normal border-pink-500 border bg-purple-800 bg-opacity-25"
-                            name="genres"
-                            id="genres"></textarea>
+                            <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
+                            type="text"
+                            name="trailer"
+                            id="trailer"
+                            :value="old('trailer')"/>
                         </div>
                     </div>
+
+                    <!--cell 2-->
+                    <div class="justify-self-center grid grid-cols-1">
+                        <!--Blurb-->
+                        <div class="self-end">
+                            <x-label for="blurb" value="Blurb"/>
+
+                            <textarea class="leading-normal border-pink-500 border bg-purple-800 bg-opacity-25"
+                            name="blurb"
+                            id="blurb"></textarea>
+                        </div>
+                    </div>
+
+                    <!--cell 3-->
+                    <div class="justify-self-center">
+                        <!--Celebrities-->
+                        <x-label for="celebrity" value="Celebrities"/>
+
+                        <textarea class="leading-normal border-pink-500 border bg-purple-800 bg-opacity-25"
+                        name="celebrities"
+                        id="celebrities"></textarea>
+                    </div>
+
+                    <!--cell 4-->
+                    <div class="justify-self-center">
+                        <!--Genre-->
+                        <x-label for="genre" value="Genres"/>
+
+                        <textarea class="leading-normal border-pink-500 border bg-purple-800 bg-opacity-25"
+                        name="genres"
+                        id="genres"></textarea>
+                    </div>
+                </div>
             </x-form>
         </div>
-
     </div>
 </x-app-layout>
--- resources/views/movies/edit.blade.php
+++ resources/views/movies/edit.blade.php
@@ -4,56 +4,92 @@
             {{ __('Edit')  }}
         </h2>
     </x-slot>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <div>
-            <x-form :action="route('movies.update', $movie)" method="POST" buttonLabel="UDPATE">
+    <div class="grid grid-cols-1">
+        <div class="w-1/2 justify-self-center">
+            <x-form :action="route('movies.update', $movie)" method="POST" buttonLabel="UPDATE">
                 @method('PUT')
                 @csrf
-                <div>
-                    <!--Title-->
-                    <div>
-                        <x-label for="title" value="Title"/>
+                <div class="flex grid grid-cols-1 gap-y-6 gap-x-2 place-content-between text-white border-pink-500 border-2 border-opacity-50 bg-purple-800 bg-opacity-25 px-4 py-6 md:grid-cols-2">
+                    <!-- cell 1-->
+                    <div class="grid grid-flow-row gap-y-2 justify-self-center">
+                        <!--Title-->
+                        <div>
+                            <x-label for="title" value="Title"/>
 
-                        <x-input type="text" name="title" id="title" :value="old('title', $movie->title)"/>
+                            <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
+                            type="text"
+                            name="title"
+                            id="title"
+                            :value="old('title', $movie->title)"/>
+
+                        </div>
+                        <!--Release Date-->
+                        <div>
+                            <x-label for="release_date" value="Release Date"/>
+
+                            <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
+                            type="date"
+                            name="release_date"
+                            id="release_date"
+                            :value="old('release_date', $movie->release_date->format('Y-m-d'))"/>
+                        </div>
+                        <!--Poster-->
+                        <div>
+                            <x-label for="poster" value="Poster"/>
+
+                            <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
+                            type="text"
+                            name="poster"
+                            id="poster"
+                            :value="old('poster', $movie->poster)"/>
+                        </div>
+                        <!--Trailer-->
+                        <div>
+                            <x-label for="trailer" value="Trailer"/>
+
+                            <x-input class="border-pink-500 border bg-purple-800 bg-opacity-25"
+                            type="text"
+                            name="trailer"
+                            id="trailer"
+                            :value="old('trailer', $movie->trailer)"/>
+                        </div>
                     </div>
 
-                    <!--Release Date-->
-                    <div>
-                        <x-label for="release_date" value="Release Date"/>
+                    <!--cell 2-->
+                    <div class="justify-self-center grid grid-cols-1">
+                        <!--Blurb-->
+                        <div class="self-end">
+                            <x-label for="blurb" value="Blurb"/>
 
-                        <x-input type="date" name="release_date" id="release_date" :value="old('release_date', $movie->release_date->format('Y-m-d'))"/>
+                            <textarea class="leading-normal border-pink-500 border bg-purple-800 bg-opacity-25"
+                            name="blurb"
+                            id="blurb"> {{$movie->blurb}}</textarea>
+                        </div>
                     </div>
 
-                    <!--Poster-->
-                    <div>
-                        <x-label for="poster" value="Poster"/>
+                    <!--cell 3-->
+                    <div class="justify-self-center">
+                        <!--Celebrities-->
+                        <x-label for="celebrity" value="Celebrities"/>
 
-                        <x-input type="text" name="poster" id="poster" :value="old('poster', $movie->poster)"/>
+                        <textarea class="leading-normal border-pink-500 border bg-purple-800 bg-opacity-25"
+                        name="celebrities"
+                        id="celebrities"> Existing Celebrities</textarea>
                     </div>
 
-                    <!--Trailer-->
-                    <div>
-                        <x-label for="trailer" value="Trailer"/>
+                    <!--cell 4-->
+                    <div class="justify-self-center">
+                        <!--Genre-->
+                        <x-label for="genre" value="Genres"/>
 
-                        <x-input type="text" name="trailer" id="trailer" :value="old('trailer', $movie->trailer)"/>
-                    </div>
-
-                    <!--Blurb-->
-                    <div>
-                        <x-label for="blurb" value="Blurb"/>
-
-                        <textarea name="blurb" id="blurb"> {{$movie->blurb}}</textarea>
+                        <textarea class="leading-normal border-pink-500 border bg-purple-800 bg-opacity-25"
+                        name="genres"
+                        id="genres"> Existing Genres</textarea>
                     </div>
                 </div>
             </x-form>
         </div>
-    </x-auth-card>
+    </div>
 </x-app-layout>
 
 
