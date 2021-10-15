b30ab5a711f458fa25952695aa613d5f7f99da2d
vrodriguero <vickyamauti@hotmail.com>
2021-10-15 14:54:55 +1000

    fix link

--- resources/views/celebrities/show.blade.php
+++ resources/views/celebrities/show.blade.php
@@ -13,18 +13,18 @@
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
                             <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                        </svg>    
-                        EDIT 
+                        </svg>
+                        EDIT
                     </button>
                 </a>
                 <form method="POST">
                     @csrf
                     @method('DELETE')
-                    <button class="flex items-center bg-pink-300 text-black rounded-xl p-2"> 
+                    <button class="flex items-center bg-pink-300 text-black rounded-xl p-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                        </svg> 
-                        DELETE 
+                        </svg>
+                        DELETE
                     </button>
                 </form>
             </div>
@@ -51,13 +51,15 @@
         </div>
     </div>
     <div class="text-3xl italic mt-10">
-        Known for: 
+        Known for:
         <div class="flex space-x-2 mt-5">
         @foreach ($celebrity->movies as $movie)
-		    <img class= "object-cover w-1/6" src="{{ url($movie->poster) }}" alt="poster">
+            <a href="{{ route('movies.show', $movie->id) }}" class="w-1/6">
+                <img class= "object-cover" src="{{ url($movie->poster) }}" alt="poster">
+            </a>
         @endforeach
         </div>
-    </div> 
+    </div>
 </div>
 
 </x-app-layout>
